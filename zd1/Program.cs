a9fa6c1e27c7ba3a7019442e6995712357a49570
Nikita <nekit.im@yandex.ru>
2022-10-29 20:53:04 +0300

    all good

--- zd1/Program.cs
+++ zd1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Text;
+using System.Drawing;
+
+class call_dll
+{
+    [DllImport("liba.dll")]
+    static extern unsafe char* ChengeRegisterLettersInArray(char* arrayLetters, int n);
+    [DllImport("liba.dll")]
+    static extern void Write();
+
+
+    public static void Main()
+    {
+        unsafe
+        {
+            int size = 6;
+            char* listLetters = stackalloc char[size];
+            listLetters[0] = 'r';
+            listLetters[1] = 'A';
+            listLetters[2] = 'K';
+            listLetters[3] = 'z';
+            listLetters[4] = 'V';
+            listLetters[5] = 'm';
+            char* a = ChengeRegisterLettersInArray(listLetters, size);
+            Write();
+
+        }
+    }
+}
