73b43f5d38865e1966585b834fffb7715e7f771d
Nikita <nekit.im@yandex.ru>
2022-11-04 14:59:55 +0300

    end

--- zd1/Program.cs
+++ zd1/Program.cs
@@ -1,31 +1,30 @@
 ﻿using System.Runtime.InteropServices;
-using System;
-using System.Text;
-using System.Drawing;
 
 class call_dll
 {
     [DllImport("liba.dll")]
-    static extern unsafe char* ChengeRegisterLettersInArray(char* arrayLetters, int n);
-    [DllImport("liba.dll")]
-    static extern void Write();
-
+    static extern unsafe sbyte *ChengeRegisterLettersInArray(sbyte *arrayLetters, int n);
 
     public static void Main()
     {
         unsafe
         {
             int size = 6;
-            char* listLetters = stackalloc char[size];
-            listLetters[0] = 'r';
-            listLetters[1] = 'A';
-            listLetters[2] = 'K';
-            listLetters[3] = 'z';
-            listLetters[4] = 'V';
-            listLetters[5] = 'm';
-            char* a = ChengeRegisterLettersInArray(listLetters, size);
-            Write();
-
+            sbyte[] listLetters = new sbyte[size];
+            listLetters[0] = (sbyte)'r';
+            listLetters[1] = (sbyte)'A';
+            listLetters[1] = (sbyte)'A';
+            listLetters[2] = (sbyte)'K';
+            listLetters[3] = (sbyte)'z';
+            listLetters[4] = (sbyte)'V';
+            listLetters[5] = (sbyte)'m';
+            
+            fixed (sbyte* p = &listLetters[0])
+            {
+                sbyte* a = ChengeRegisterLettersInArray(p, size);
+                string abyr = new string(a);
+                Console.WriteLine(abyr);
+            }
         }
     }
 }
